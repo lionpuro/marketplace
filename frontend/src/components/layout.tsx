0c4fa36b9e46aaf87ee02fe4738ed5081c1df428
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-08-13 08:54:48 +0300

    add signup link to nav

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -16,31 +16,41 @@ const Layout = ({ children }: { children?: ReactNode }) => {
 						>
 							marketplace
 						</Link>
-						{!isAuthenticated ? (
-							<Link
-								to="/signin"
-								className="ml-auto bg-primary-400 text-neutral-50 py-1.5 px-4"
-							>
-								Sign in
-							</Link>
-						) : (
-							<div className="ml-auto flex items-center gap-2">
-								<Link
-									to="/account"
-									className="flex items-center gap-1 text-primary-700 p-1.5"
-								>
-									<IconUser className="text-lg" />
-									Account
-								</Link>
-								<button
-									onClick={signOut}
-									className="flex items-center gap-1 text-red-600/90 py-1.5 px-4"
-								>
-									<IconSignOut className="text-lg" />
-									Sign out
-								</button>
-							</div>
-						)}
+						<div className="ml-auto flex items-center gap-2">
+							{!isAuthenticated ? (
+								<>
+									<Link
+										to="/signin"
+										className="ml-auto border-2 border-primary-400 bg-primary-400 text-neutral-50 py-1 px-4"
+									>
+										Sign in
+									</Link>
+									<Link
+										to="/signup"
+										className="ml-auto border-2 border-primary-400 text-primary-500 font-medium py-1 px-4"
+									>
+										Sign up
+									</Link>
+								</>
+							) : (
+								<>
+									<Link
+										to="/account"
+										className="flex items-center gap-1 text-primary-700 p-1.5"
+									>
+										<IconUser className="text-lg" />
+										Account
+									</Link>
+									<button
+										onClick={signOut}
+										className="flex items-center gap-1 text-red-600/90 py-1.5 px-4"
+									>
+										<IconSignOut className="text-lg" />
+										Sign out
+									</button>
+								</>
+							)}
+						</div>
 					</nav>
 				</header>
 				<div className="flex flex-col p-8 grow">{children}</div>
