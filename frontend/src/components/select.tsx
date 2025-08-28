945ce109b172a074d164bf74b80084c7bcb771c4
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-08-28 14:41:08 +0300

    fix select component focus class

--- frontend/src/components/select.tsx
+++ frontend/src/components/select.tsx
@@ -4,7 +4,14 @@ type Props = SelectHTMLAttributes<HTMLSelectElement>;
 
 export const Select = ({ children, className, ...props }: Props) => (
 	<select
-		className={`bg-white disabled:bg-neutral-200/50 px-3 py-1.5 pr-3 border-r-5 border-transparent outline outline-neutral-200 focus-within:outline-2 focus-within:-outline-offset-1 focus-within:outline-focus${className ? " " + className : ""}`}
+		className={[
+			"bg-white disabled:bg-neutral-200/50 px-3 py-1.5 pr-3",
+			"border-r-5 border-transparent outline outline-neutral-200",
+			"focus-within:outline-2 focus-within:-outline-offset-1 focus-within:outline-focus",
+			className,
+		]
+			.join(" ")
+			.trim()}
 		{...props}
 	>
 		{children}
